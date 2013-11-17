003433c817a9f43933902821025bdbb7c25fb243
Sergey Shutka <shutka.sergey@gmail.com>
2013-11-17 18:02:13 +1100

    Restored IoC container (with support for MVC 5).
    Restored NUnit and NSubstitute for Multifive.Web.Tests project.

---
+++ MultiFive.Web.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using NUnit.Framework;
+
+using MultiFive.Web.Controllers;
+
+namespace MultiFive.Web.Tests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        [Test]
+        public void IndexReturnsNonNullView()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
