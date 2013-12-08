4864d480c78b14419d3d62f1ccbe330a87c80650
Tagir Magomedov <tagir.magomedov@gmail.com>
2013-12-08 00:12:01 -0800

    Added xml comments from stateless.cs to stateless.js

--- MultiFive.Web/Scripts/stateless.js
+++ MultiFive.Web/Scripts/stateless.js
@@ -15,12 +15,29 @@
 
         self.handlers = {};
 
-        self.permit = function(trigger, state) {
-            self.handlers[trigger] = state;
+        self.permit = function (trigger, destinationState) {
+            /// <summary>
+            /// Accept the specified trigger and transition to the destination state.
+            /// </summary>
+            /// <param name="trigger">The accepted trigger.</param>
+            /// <param name="destinationState">The state that the trigger will cause a transition to.</param>
+            /// <returns>The reciever.</returns>
+            
+            self.handlers[trigger] = destinationState;
+            return self;
         };
 
-        self.permitDynamic = function(trigger, stateSelector) {
-            self.handlers[trigger] = stateSelector;
+        self.permitDynamic = function (trigger, destinationStateSelector) {
+            /// <summary>
+            /// Accept the specified trigger and transition to the destination state, calculated
+            /// dynamically by the supplied function.
+            /// </summary>
+            /// <param name="trigger">The accepted trigger.</param>
+            /// <param name="destinationStateSelector">Function to calculate the state that the trigger will cause a transition to.</param>
+            /// <returns>The reciever.</returns>
+            
+            self.handlers[trigger] = destinationStateSelector;
+            return self;
         };
 
         self.on = function(trigger, params) {
@@ -50,9 +67,17 @@
         self.currentState = initialState;
         self.states = {};
 
-        self.fire = function(trigger, params) {
+        self.fire = function(trigger, p) {
+            /// <summary>
+            /// Transition from the current state via the specified trigger.
+            /// The target state is determined by the configuration of the current state.
+            /// Actions associated with leaving the current state and entering the new one
+            /// will be invoked.
+            /// </summary>
+            /// <param name="trigger">The trigger to fire.</param>
+            /// <param name="p">The argument to be passed to state selector (only implemented for permitDynamic).</param>
 
-            var newState = self.currentState.on(trigger, params);
+            var newState = self.currentState.on(trigger, p);
 
             if (newState) {
 
