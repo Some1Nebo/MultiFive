e0e0310d82c362c7c96a8e703fef93f24957d1bb
Tagir Magomedov <tagir.magomedov@gmail.com>
2013-12-07 22:04:42 -0800

    Showing game field in /game/show and data-binded cells to values.

--- MultiFive.Web/Scripts/Show/show.js
+++ MultiFive.Web/Scripts/Show/show.js
@@ -1,28 +1,59 @@
 ﻿// codebehind script for /game/show
 // module pattern
 
-var MultiFive = (function(ns) {
+var MultiFive = (function (ns) {
 
-    function ViewModel(player1Name, player2Name) {
-        this.player1Name = ko.observable(player1Name);
-        this.player2Name = ko.observable(player2Name);
+    function ViewModel(gameData, cellClicked) {
+        var self = this;
+        
+        self.player1Name = ko.observable(gameData.player1Name);
+        self.player2Name = ko.observable(gameData.player2Name);
+
+        self.field = [];
+
+        var cellView = {
+            "0": "&nbsp;",
+            "1": "X",
+            "2": "O"
+        };
+        
+        for (var i = 0; i < gameData.height; ++i) {
+            var row = [];
+
+            for (var j = 0; j < gameData.width; ++j) {
+                var initValue = cellView[gameData.fieldData[i * gameData.width + j]];
+                row.push(ko.observable(initValue));
+            }
+
+            self.field.push(row);
+        }
+
+        self.cellClicked = cellClicked;
+        
+        return self;
     }
 
-    ns.show = function(gameId, player1Name, player2Name, lastMessageId) {
+    ns.show = function (gameData) {
+        var self = this;
 
-        var viewModel = new ViewModel(player1Name, player2Name);
+        var cellClicked = function (r, c) {
+            viewModel.field[r][c]("X");
+        };
+
+        var viewModel = new ViewModel(gameData, cellClicked);
         ko.applyBindings(viewModel);
 
-        var messageHub = new ns.MessageHub(gameId, lastMessageId);
+        var messageHub = new ns.MessageHub(gameData.gameId, gameData.lastMessageId);
 
         messageHub.hooks.joined = function(messageData) {
 
             viewModel.player2Name(messageData.joinedPlayerName);
 
         };
-
+        
         messageHub.listen();
 
+        return self;
     };
 
     return ns;
